a3f890809370e1d60db7dc9f93d5982d5d9d2e2e
melerokristy <165970194+melerokristy@users.noreply.github.com>
2024-06-08 14:12:51 -0500

    changes generating

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a Spanish poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">⏳ Generating brunch ideas... ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
